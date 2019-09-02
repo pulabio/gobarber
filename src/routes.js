c31b7f3e399a533050061f5be7af0f476ed7bb59
pulabio <pulabio@gmail.com>
2019-09-02 00:25:25 -0300

    testing database on route (will be changed)

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
-import Router from 'express';
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Flávio',
+    email: 'asdf@asdf.com',
+    password_hash: 'asdfasdf',
+  });
+
+  res.json({ user });
+});
 
 export default routes;
